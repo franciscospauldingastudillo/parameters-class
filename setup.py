174494280c38da5b7891520b445e7aa15fdc8b80
Dr. Francisco Spaulding-Astudillo <fspauldinga@ucla.edu>
2025-03-12 15:58:48 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parameters",
+    version="0.1",
+    packages=find_packages(),
+    description="thermo-spectral parameters configuration class",
+    author="Francisco Spaulding-Astudillo",
+    license="MIT",
+    install_requires=[],
+)
